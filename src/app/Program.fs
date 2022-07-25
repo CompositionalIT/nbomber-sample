a9707aab212fe1c8af8fb8c020cbe124e8586ec6
isaac <isaac.abraham@gmail.com>
2022-07-25 21:42:37 +0100

    Initial commit

---
+++ src/app/Program.fs
@@ -0,0 +1,9 @@
+﻿open Saturn
+open Giraffe
+open Microsoft.Extensions.Hosting
+open Microsoft.AspNetCore.Http
+
+let routes = text "Hello World!"
+let app = application { use_router routes }
+
+run app
---
+++ src/testrig/Program.fs
@@ -0,0 +1,31 @@
+﻿open NBomber.FSharp
+open NBomber.Contracts
+open NBomber.Contracts.Stats
+open System.Net.Http
+open System
+open NBomber
+
+let client = new HttpClient()
+
+let step =
+    Step.create (
+        "My First Test",
+        fun _ ->
+            task {
+                let! response = client.GetAsync "http://localhost:5000/"
+                let! content = response.Content.ReadAsByteArrayAsync()
+
+                return
+                    if response.IsSuccessStatusCode then
+                        Response.ok (content, int response.StatusCode)
+                    else
+                        Response.fail (response.ReasonPhrase, int response.StatusCode, content.Length)
+            }
+    )
+
+Scenario.create "My First Scenario" [ step ]
+|> Scenario.withoutWarmUp
+|> Scenario.withLoadSimulations [ InjectPerSecRandom(100, 500, seconds 20) ]
+|> NBomberRunner.registerScenario
+|> NBomberRunner.run
+|> ignore
